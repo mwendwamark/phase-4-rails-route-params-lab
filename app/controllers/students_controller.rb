0565011e96ae0db4b781ef641284d3c41782685c
mark mwendwa <mwendwamark310@gmail.com>
2023-06-19 13:22:11 +0300

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
     render json: students
   end
 
+  def show 
+    show = Student.find_by(id: params[:id])
+    render json: show
+  end
 end
